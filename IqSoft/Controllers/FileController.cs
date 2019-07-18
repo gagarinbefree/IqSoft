e8866dcc37a6d93220d73539bae9f54ac183f5b8
gagarinbefree <gagarinbefree@gmail.com>
2019-07-18 12:30:33 +0500

    first commit

---
+++ IqSoft/Controllers/FileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IqSoft.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PostXlsx(IFormFile file)
+        {
+            try
+            {
+                if (file == null)
+                    throw new Exception("Empty file");
+
+                if (file.Length == 0)
+                    throw new Exception("Empty file");
+
+                var filePath = Path.GetTempFileName();
+
+                if (file.Length > 0)
+                {
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                }
+
+
+                return Ok(new { file.Length, file.FileName });
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+    }
+}
--- IqSoft/Startup.cs
+++ IqSoft/Startup.cs
@@ -25,6 +25,7 @@ namespace IqSoft
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerDocument();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +35,9 @@ namespace IqSoft
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
 
             app.UseMvc();
         }
